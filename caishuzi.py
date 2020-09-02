74d007cf0851b5c7dda103e05b2f4c0d53b69746
ForgetDust <49586698+ForgetDust@users.noreply.github.com>
2020-09-02 14:01:45 +0800

    Add files via upload

---
+++ caishuzi.py
@@ -0,0 +1,25 @@
+# ！/usr/bin/env python3
+# -*- coding = utf-8 -*-
+
+"""
+@File : caushuzi.py
+@Author : ForgetDust
+@Time : 2020/9/2 11:20
+"""
+
+import random
+
+s = random.randint(1,100)
+count = 0
+a = int(input('please input a number you gest in five chance:'))
+while a != s:
+    count = count+1
+    if a >= s and count < 5:
+        a = int(input("sorry! you are worry!it's too large please try again:"))
+    elif a <= s:
+        a = int(input("sorry! you are worry!it's too small,please try again:"))
+    else:
+        print('sorry! you have no chance')
+        break
+else:
+    print('你真辛运！猜对了')
