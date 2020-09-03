6c762365905cda1f31e577e82b8ce3af7ea5fec2
ForgetDust <49586698+ForgetDust@users.noreply.github.com>
2020-09-03 09:37:57 +0800

    Add files via upload

---
+++ liebiaocunchu.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+user = ''
+
+list_user = []
+
+
+while user != 'q':
+    user = input('please input:')
+    list_user.append(user)
+    print(list_user)
+else:
+    print('退出！')
