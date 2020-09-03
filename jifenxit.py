33ac58170f152dad02795ddecfba69d31fa09308
ForgetDust <49586698+ForgetDust@users.noreply.github.com>
2020-09-03 21:45:46 +0800

    Add files via upload

--- jifenxit.py
+++ jifenxit.py
@@ -1,13 +1,23 @@
-# -*- coding: utf-8 -*-
+# ！/usr/bin/env python3
+# -*- coding = utf-8 -*-
+
 """
-Created on Thu Sep  3 09:46:30 2020
-
-@author: ForgetDust
+@File : jifenxit.py
+@Author : ForgetDust
+@Time : 2020/9/3 16:51
 """
 
-team1 = input('please input team1 name:')
-team2 = input('please input team2 name:')
 
+team1 = input('please input team1 name:').strip()
+if team1 == 'game over':
+    team1 = input('please input team1 name:').strip()
+
+team2 = input('please input team2 name:').strip()
+if team2 == 'game over':
+    team2 = input('please input team2 name:').strip()
+
+if team1 == team2:
+    team2 = input('please input another team2 name:').strip()
 
 list_score1 = []
 list_score2 = []
@@ -20,42 +30,41 @@ user = ''
 while user != 'game over':
     user = input('please input team:')
     if user == team1:
-        score1 = input('please input score:')
-        if score1.isdigit() == 'true':
+        score1 = input('please input score:').strip()
+        if score1.isdigit() == True:
             score1 = int(score1)
             if score1 <= 3:
                 list_score1.append(score1)
                 totle1 = sum(list_score1)
-                print('team %s is scored %d   team %s is scored %d' %(team1,totle1,team2,totle2))
+                print('team %s is scored %d   team %s is scored %d' % (team1, totle1, team2, totle2))
             else:
                 print('The score you entered is illegal')
         else:
             print('The score you entered is illegal')
-  
+
     elif user == team2:
-        score2 = input('please input score:')
-        if score2.isdigit() == 'true':
+        score2 = input('please input score:').strip()
+        if score2.isdigit() == True:
             score2 = int(score2)
             if score2 <= 3:
                 list_score2.append(score2)
                 totle2 = sum(list_score2)
-                print('team %s is scored %d   team %s is scored %d' %(team1,totle1,team2,totle2))
+                print('team %s is scored %d   team %s is scored %d' % (team1, totle1, team2, totle2))
             else:
                 print('The score you entered is illegal')
         else:
             print('The score you entered is illegal')
-        
+
     else:
         print('your input error! please try again!')
-        
+
 else:
     print("GAME OVER!")
-    print('team %s scored %d' %(team1,totle1))
-    print('team %s scored %d' %(team2,totle2))
+    print('team %s scored %d' % (team1, totle1))
+    print('team %s scored %d' % (team2, totle2))
     if totle1 > totle2:
-        print('Winer is %s' %team1)
+        print('Winer is %s' % team1)
     elif totle1 < totle2:
-        print('Winer is %s' %team2)
+        print('Winer is %s' % team2)
     else:
         print("it ends in a draw")
-        
