532bc5e3d7e9571d734f1f1c6e6a6160a0449271
ForgetDust <49586698+ForgetDust@users.noreply.github.com>
2020-09-10 10:44:56 +0800

    Add files via upload

---
+++ chufayichang.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 10:39:42 2020
+
+@author: tute
+"""
+
+try:
+    x = input("被除数：")
+    y = input("除数：")
+    z = int(x) / int(y)
+    
+except ValueError:
+    print("请输入数字")
+except ZeroDivisionError:
+    print("除数不能为0")
+else:
+    print(x,"/",y,"=",z)
